44bbd522cdbffa33257538b3a82977f49da2c3f1
MagentaSpruce <81999424+MagentaSpruce@users.noreply.github.com>
2021-07-08 19:11:38 -0400

    Updates

---
+++ precache-manifest.3f564a6163415a66723ec88b386aa5f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5d5ddc2858544919aa67af7e7930bef",
+    "url": "/reactNavbar/index.html"
+  },
+  {
+    "revision": "85d881a23ff6fa484087",
+    "url": "/reactNavbar/static/css/main.d1743f3b.chunk.css"
+  },
+  {
+    "revision": "7c7331f0131e2cf50072",
+    "url": "/reactNavbar/static/js/2.af817d26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactNavbar/static/js/2.af817d26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85d881a23ff6fa484087",
+    "url": "/reactNavbar/static/js/main.c6382fb0.chunk.js"
+  },
+  {
+    "revision": "dacac0e9a161d02464f9",
+    "url": "/reactNavbar/static/js/runtime-main.63d8423c.js"
+  }
+]);
